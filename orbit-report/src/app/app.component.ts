590f5c90774abb6a45b613d805e9fefe1035648b
Alex Cooper <alexcooper@kc.rr.com>
2019-12-18 18:49:16 -0600

    Completed section 6 Highlight Space Debris

--- orbit-report/src/app/app.component.ts
+++ orbit-report/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Satellite } from './satellite';
+import { JsonPipe } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'orbit-report';
+  sourceList: Satellite[];
+
+  constructor() {
+    this.sourceList = [];
+    let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
+ 
+    window.fetch(satellitesUrl).then(function(response) {
+       response.json().then(function(data) {
+
+        let fetchedSatellites = data.satellites;
+        let i=0;
+        while(i<fetchedSatellites.length){
+          this.sourceList.push(new Satellite(
+            fetchedSatellites[i].name,
+            fetchedSatellites[i].type, 
+            fetchedSatellites[i].launchDate, 
+            fetchedSatellites[i].orbitType, 
+            fetchedSatellites[i].operational
+          ));
+          i+=1
+        }
+  
+       }.bind(this));
+    }.bind(this));
+ 
+ }
 }
